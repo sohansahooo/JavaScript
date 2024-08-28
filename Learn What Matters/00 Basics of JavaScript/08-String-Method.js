f7607d079cd1bbff64026bf1729396401e685cd2
sohanxsahoo <sohanxsahoo@gmail.com>
2024-08-28 19:07:29 +0530

    update string methods

--- Learn What Matters/00 Basics of JavaScript/08-String-Method.js
+++ Learn What Matters/00 Basics of JavaScript/08-String-Method.js
@@ -1,4 +1,6 @@
-/* "Content, "blah blah blah..."" ✖️
+/* You can write like this in a string to avoid writing complication:
+
+   "Content, "blah blah blah..."" ✖️
    "Content, \"blah blah blah...\"" ✔️ (\" escaping)
 */
 
@@ -15,8 +17,8 @@ console.log(`The username is ${username}, age is ${age}, address is ${address} a
 
 
 /* length: returns the length of the string */
-let a = " Hey! How are you. "
-console.log(a.length); // 13
+let a = "   Hey! How are you.   "
+console.log(a.length); // 23 letters using spaces 
 
 /* toUppercase() */
 console.log(a.toUpperCase());
@@ -36,11 +38,18 @@ let c = "RockStar"
 console.log(c.indexOf('k')); // returns the index of the first occurrence of the specified value
 
 /* replace() */
-let d = "Hello, how are you doing today?"
-console.log(d.replace('how', 'why')); // returns a new string with the specified value replaced
+let d = "https://example.com/example%20something"
+console.log(d.replace('%20', '-')); // returns a new string with the specified value replaced
+
+/* includes() */
+let e = "Hello, how are you doing today?"
+console.log(e.includes('doing')); // returns true if the string includes the specified value
 
 /* substring() */
-let e = "Hello, how are you doing today?"
-console.log(e.substring(7, 14)); // returns the characters in a string between two specified inclusive and exclusive index
+let f = "Hello, how are you doing today?"
+console.log(f.substring(7, 14)); // returns the characters in a string between two specified inclusive and exclusive index
 
-// substring() vs slice()
+/* substring() vs slice()
+   -- substring() is inclusive of the start index and exclusive of the end index
+   -- slice() is inclusive of both the start and end index
+*/
