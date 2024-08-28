aaee6445ee14a1fa43aa1c30f12781ecb10d3937
sohanxsahoo <sohanxsahoo@gmail.com>
2024-08-28 11:51:10 +0530

    update

--- practice.js
+++
@@ -1,8 +0,0 @@
-let yourName;
-yourName = prompt("Enter your username:"); // Window Prompt
-console.log(yourName);
-
-document.getElementById('submit').onclick = function(){
-    yourName = document.getElementById('inp').value;
-    document.getElementById('const').textContent = `Welcome ${yourName}`
-};
