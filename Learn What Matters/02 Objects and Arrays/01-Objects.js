47007336f62515708f8232520f3f31bb0054dd4d
sohanxsahoo <sohanxsahoo@gmail.com>
2024-08-28 21:44:50 +0530

    update object

--- Learn What Matters/02 Objects and Arrays/01-Objects.js
+++ Learn What Matters/02 Objects and Arrays/01-Objects.js
@@ -1,20 +1,23 @@
 // +++++++++++++++++++++++++++++++++++++++++ Objects +++++++++++++++++++++++++++++++++++++++++
-
+/* Types:
+-- Singleton
+-- Object Literals (we discuss this one)
+*/
 
 function anArray() {
-    const user = [
-      { name: "John", age: 25, gender: "male" },
-      { name: "Jany", age: 22, gender: "female" },
-      { name: "Jerra", age: 22, gender: "female" },
-      { name: "Jwel", age: 21, gender: "female" },
-      { name: "Jizz", age: 26, gender: "female" }
-    ];
+  const user = [
+    { name: "John", age: 25, gender: "male" },
+    { name: "Jany", age: 22, gender: "female" },
+    { name: "Jerra", age: 22, gender: "female" },
+    { name: "Jwel", age: 21, gender: "female" },
+    { name: "Jizz", age: 26, gender: "female" }
+  ];
   
-    return user[0];
-  }
-  
-  const firstUser = anArray();
-  console.log(firstUser, 'objects & their values');
+  return user[3];
+}
+
+const firstUser = anArray();
+console.log(firstUser, '< (key & their values)');
 
 
 // Pass Function inside Object:
@@ -27,4 +30,19 @@ let user = {
   }
 };
 
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
 user.intro()
+console.log(user.hasOwnProperty('city'));
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++ Spread Operator +++++++++++++++++++++++++++++++++++++++++
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+const spread_obj = {...obj1, ...obj2, ...obj3}
+console.log(spread_obj)
