93ecd13d874104a9cfecc751f886a6c200fc51a1
sohanxsahoo <sohanxsahoo@gmail.com>
2025-01-08 19:50:13 +0530

    Add driving eligibility challenge to coding exercises

---
+++ 01 Do Coding Exercise/Challenge 2/assignment-1.js
@@ -0,0 +1,14 @@
+/*
+1. Driving Eligibility
+    -- Task: Write a program to check if a person can apply for a driving license based on age.
+
+    -- Input: age (an integer representing the person's age) and min_driving_age (an integer representing the minimum driving age)
+    -- Output: "Eligible for a driving license" or "Not eligible for a driving license"
+
+Example:
+    Input: age = 17, min_driving_age = 16
+    Output: "Eligible for driving license"
+
+    Input: age = 14, min_driving_age = 16
+    Output: "Not eligible for driving license"
+*/
